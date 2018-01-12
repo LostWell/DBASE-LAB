1b8bc0ccb9a779f80f00d42b40fe91b0c91b5e00
Shayrandrea <35225573+Shayrandrea@users.noreply.github.com>
2018-01-12 17:32:08 +0800

    Update DBController.java

--- Enrollment/src/studentrecords/DBController.java
+++ Enrollment/src/studentrecords/DBController.java
@@ -126,7 +126,72 @@ public class DBController {
         return statement.executeQuery(sql);
     }
     
-//------------------------------------------------------------------------------
+//---------------------------UPDATE FUNCTIONALITY---------------------------------------------------
+	 // Gerichelle
+    public void updateAccount(int idno,
+                              String lname,
+                              String fname,
+                              String midInitial,
+                              String gender,
+                              String contactNumber,
+                              String email) throws Exception {
+    
+        //execute a query (update preparedStatement)
+        sql = "UPDATE account"
+            + "(idno, lname, fname, midInitial, gender, contactNumber, email) "
+            + "values (?,?,?,?,?,?,?)";
+        ps = connection.prepareStatement(sql);
+        ps.setInt(1, idno);
+        ps.setString(2, lname);
+        ps.setString(3, fname);
+        ps.setString(4, midInitial);
+        ps.setString(5, gender);
+        ps.setString(6, contactNumber);
+        ps.setString(7, email);
+        ps.executeUpdate(sql);
+    }
+    // Gerichelle
+    public void updateSubject(String id,
+                              String title,
+                              int units) throws Exception {
+        sql = "UPDATE subject"
+            + "(id, title, units)"
+            + "values (?,?,?)";
+        ps = connection.prepareStatement(sql);
+        ps.setString(1, id);
+        ps.setString(2, title);
+        ps.setInt(3, units);
+        ps.executeUpdate(sql);
+    }  
+     //Gerichelle, Shaira
+    public void updateClass(String code, 
+                            String time,
+                            String days,
+                            String id) throws Exception {
+        sql = "UPDATE class"
+            + "(code, time, days,  id)"
+            + "values (?,?,?,?,?)";
+        ps = connection.prepareStatement(sql);
+        ps.setString(1, code);
+        ps.setString(2, time);
+        ps.setString(3, days);
+        ps.setString(4, id);
+        ps.executeUpdate(sql);
+    }
+    // Gerichelle
+    public void updateInfo(String code,
+                           int idno,
+                           Date date) throws Exception {
+        sql = "UPDATE info"
+            + "(code, idno, date)"
+            + "values (?,?,?)";
+        ps = connection.prepareStatement(sql);
+        ps.setString(1, code);
+        ps.setInt(2, idno);
+        ps.setDate(3, date);
+        ps.executeUpdate(sql);
+    }
+	
 	//-------------------------Extra Feature---------------------------------------------//
     //retrieves the information of each student enrolled in a given class (Henry)
     public ResultSet getClassStudent(int classCode) throws Exception {
